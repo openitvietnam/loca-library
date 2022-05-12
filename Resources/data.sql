929e7b0314efa8131026db59e01e61dc9359c9c4
tonghoangvu <tonghoangvu@outlook.com>
2022-05-12 23:27:44 +0700

    feat: add init SQL scripts

---
+++ Resources/data.sql
@@ -0,0 +1,32 @@
+﻿USE LocaLibrary;
+
+-- Empty tables with foreign keys (reverse order)
+DELETE FROM BookBorrow;
+DELETE FROM BookCategory;
+PRINT 'Empty tables with foreign keys';
+
+-- Empty tables without foreign keys (reverse order)
+DELETE FROM Employee;
+DELETE FROM Member;
+DELETE FROM Book;
+DELETE FROM Category;
+PRINT 'Empty tables without foreign keys';
+
+-- Reset id for tables with foreign keys (reverse order)
+DBCC CHECKIDENT ('BookBorrow', RESEED, 0);
+-- DBCC CHECKIDENT ('BookCategory', RESEED, 1); -- don't have id
+PRINT 'Reset id for tables with foreign keys';
+
+-- Reset id for tables without foreign keys (reverse order)
+DBCC CHECKIDENT ('Employee', RESEED, 0);
+DBCC CHECKIDENT ('Member', RESEED, 0);
+DBCC CHECKIDENT ('Book', RESEED, 0);
+DBCC CHECKIDENT ('Category', RESEED, 0);
+PRINT 'Reset id for tables without foreign keys';
+
+-- Insert Admin employee
+INSERT INTO Employee(Email, PasswordHash, FullName)
+    VALUES ('admin@gmail.com', PWDENCRYPT(N'123456'), N'Admin');
+PRINT 'Inserted employees';
+
+USE master;
---
+++ Resources/setup.sql
@@ -0,0 +1,27 @@
+﻿USE master;
+
+-- Drop previous data
+IF EXISTS (SELECT name FROM sys.sql_logins WHERE name = 'LocaLibrary')
+BEGIN
+    DROP LOGIN LocaLibrary;
+END;
+DROP DATABASE IF EXISTS LocaLibrary;
+DROP USER IF EXISTS LocaLibrary;
+PRINT 'Dropped previous data';
+
+-- Create login & database (need GO)
+CREATE LOGIN LocaLibrary WITH PASSWORD = '123456', CHECK_EXPIRATION = OFF;
+CREATE DATABASE LocaLibrary;
+GO
+PRINT 'Created login & database';
+
+-- Create user (in database)
+USE LocaLibrary;
+CREATE USER LocaLibrary FOR LOGIN LocaLibrary;
+PRINT 'Created user';
+
+-- Set user as database owner
+EXEC sp_addrolemember 'db_owner', 'LocaLibrary';
+PRINT 'Set user as database owner';
+
+USE master;
