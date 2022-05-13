a02032bf796f514695291c6e169de0844224f790
tonghoangvu <tonghoangvu@outlook.com>
2022-05-13 11:41:51 +0700

    chore: verify login password

--- LocaLibrary.App/Forms/LoginForm.cs
+++ LocaLibrary.App/Forms/LoginForm.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 using LocaLibrary.App.Services;
 
 namespace LocaLibrary.App.Forms
@@ -45,12 +47,39 @@ namespace LocaLibrary.App.Forms
             return null;
         }
 
+        private string validateCredentials()
+        {
+            var sql = @"SELECT TOP 1 Id, Email, FullName, IsAdmin, IsLocked
+                        FROM Employee
+                        WHERE Email = @Email AND PWDCOMPARE(@Password, PasswordHash) = 1";
+            var command = DatabaseService.CreateCommand(sql, CommandType.Text);
+            command.Parameters.AddWithValue("@Email", inputEmail.Text);
+            command.Parameters.AddWithValue("@Password", inputPassword.Text);
+
+            var dataTable = DatabaseService.GetDataTable(command);
+            if (dataTable.Rows.Count == 0)
+            {
+                return "Email and password do not match";
+            }
+            if (dataTable.Rows[0]["IsLocked"].ToString() == "True")
+            {
+                return "Account is locked";
+            }
+            return null;
+        }
+
         private void buttonLogin_Click(object sender, EventArgs e)
         {
             var error = validateForm();
             if (error != null)
             {
                 MessageBox.Show(error, "Error");
+                return;
+            }
+            var credentialsError = validateCredentials();
+            if (credentialsError != null)
+            {
+                MessageBox.Show(credentialsError, "Error");
                 return;
             }
             OpenMainForm();
